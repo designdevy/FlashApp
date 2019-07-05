43fd6245cf2fa427ff2a04b5b0aa5254344fb34b
nataliaev <nataliiaevchenko@gmail.com>
2019-07-05 11:02:23 +0200

    Add random function

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const cardCollection = [{foto : "https://www.guidedogs.org/wp-content/uploads/2018/01/Mobile.jpg", title : "hond", answer : "dog"}, 
+const cardCollection = [{foto : "https://cdn.pixabay.com/photo/2017/09/25/13/12/dog-2785074_960_720.jpg", title : "hond", answer : "dog"}, 
                         {foto : "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/erik-jan-leusink-561776-unsplash-1539591981.jpg?resize=480:*", title : "kat", answer : "cat"}, 
-                        {foto : "", answer : "dog"}, 
-                        {foto : "", answer : "dog"}, 
-                        {foto : "", answer : "dog"}];
+                        {foto : "https://upload.wikimedia.org/wikipedia/commons/5/54/Mexican_wolf_lounging.jpg", title : "wolf", answer : "wolk"}, 
+                        {foto : "https://www.nps.gov/lacl/learn/nature/images/Image-w-cred-cap_-1200w_-Animals-page_-Red-Fox-head-profile_2.jpg?maxwidth=1200&maxheight=1200&autorotate=false", title: "fox", answer : "vos"}, 
+                        {foto : "https://live.staticflickr.com/4007/5075169756_d51008b274_b.jpg", title : "tree", answer : "boom"},
+                        {foto : "https://live.staticflickr.com/7193/6908555819_5919b910e8_b.jpghttps://live.staticflickr.com/7193/6908555819_5919b910e8_b.jpg", title : "grass", answer : "grass"},
+                        {foto : "https://cdn12.picryl.com/photo/2016/12/31/cluster-flowers-little-flower-nature-landscapes-4027ad-1024.jpg", title : "flower", answer : "bloem"}];
 
 displayImage()
 
 function displayImage() {
-    let randomNumber = 1;
+    let randomNumber = Math.floor(Math.random()*((cardCollection.length - 1) + 1));
+    console.log(randomNumber);
     const cardGallery = document.getElementById("card");
     let link = cardCollection[randomNumber].foto;
 
